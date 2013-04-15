f8ed7f3135b54ba3419516b78dca365cbd76d0d9
Harri Salokorpi <hsalokor@iki.fi>
2013-04-15 08:46:58 +0300

    Initial client socket

--- src/client.rkt
+++ src/client.rkt
@@ -1,4 +1,17 @@
 #lang racket
 (require racket/tcp)
+(require json)
 
-(print "helo")
+(define (payload)
+  (jsexpr->string (hash 'q "x"
+                        'b "d")))
+
+(define (send-and-receive)
+  (define-values (in out) (tcp-connect/enable-break "127.0.0.1" 8000))
+  (displayln (payload))
+  (displayln (payload) out)
+  (display "\n\n" out)
+  (displayln (payload) out)
+  (displayln (read-line in)))
+
+(send-and-receive)
